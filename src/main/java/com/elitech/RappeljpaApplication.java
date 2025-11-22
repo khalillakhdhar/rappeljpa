ade962cd7b1afe665c1785ff602f17a38ae0c31a
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2025-11-22 17:41:23 +0100

    Initial commit

---
+++ src/main/java/com/elitech/RappeljpaApplication.java
@@ -0,0 +1,13 @@
+package com.elitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RappeljpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RappeljpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/elitech/model/User.java
@@ -0,0 +1,54 @@
+package com.elitech.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = false)
+	private String nom;
+	@Column(nullable = false,unique = true)
+	private String email;
+	@Column(nullable = false)
+	private String password;
+	@Column(columnDefinition = "varchar(20) default 'utilisateur' ")
+	private String role;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+}
